bf0ed770c30044a7f9197db2bc7bb86ea337788e
ChikaKakazu <kakazu1314@gmail.com>
2024-05-28 23:25:31 +0900

    GoReleaser追加

--- cmd/root.go
+++ cmd/root.go
@@ -11,14 +11,10 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go-cli-switchbot",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:     "go-cli-switchbot",
+	Version: "0.1.0",
+	Short:   "CLI tool to operate SwitchBot",
+	Long:    `CLI tool to operate SwitchBot. It can be used to control SwitchBot devices like Bot, Meter, Hub, etc.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
